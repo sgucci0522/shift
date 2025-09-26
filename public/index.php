385cd106a95c145fdd78c1a3f0f0723dc3656684
sakaguchi <sakaguchi.iu-doren.com>
2025-09-26 10:55:29 +0900

    '時刻入力8～20時に変更'

--- public/index.php
+++ public/index.php
@@ -60,14 +60,6 @@ require_once __DIR__ . '/../src/check_auth.php';
                     <label>開始時刻:</label>
                     <div class="time-select-group">
                         <select name="start-hour" required>
-                            <option value="00">00</option>
-                            <option value="01">01</option>
-                            <option value="02">02</option>
-                            <option value="03">03</option>
-                            <option value="04">04</option>
-                            <option value="05">05</option>
-                            <option value="06">06</option>
-                            <option value="07">07</option>
                             <option value="08">08</option>
                             <option value="09">09</option>
                             <option value="10">10</option>
@@ -81,9 +73,6 @@ require_once __DIR__ . '/../src/check_auth.php';
                             <option value="18">18</option>
                             <option value="19">19</option>
                             <option value="20">20</option>
-                            <option value="21">21</option>
-                            <option value="22">22</option>
-                            <option value="23">23</option>
                         </select>
                         <span>:</span>
                         <select name="start-minute" required>
